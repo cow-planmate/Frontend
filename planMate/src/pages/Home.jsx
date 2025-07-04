0240e64e30d58d5211d44fa2180c0fe079b83343
wekfj <kdh01pro@gmail.com>
2025-07-04 20:12:24 +0900

    메인페이지 버튼, 카드 css

--- planMate/src/pages/Home.jsx
+++ planMate/src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import "tailwindcss/tailwind.css";
+
 function App() {
-    return (
-        <h1>Home</h1>
-    )
+  return (
+    <div>
+      <button
+        className="cursor-pointer transition-all bg-[#1344FF] text-white px-6 py-2 rounded-lg
+border-[#1344FF]
+active:border-b-[2px] active:brightness-90 active:translate-y-[2px] "
+      >
+        일정생성하기
+      </button>
+
+      <div className="w-[1150px] h-[100px] mx-auto bg-gray-100 rounded-xl shadow-2xl pl-50"></div>
+    </div>
+  );
 }
 
 export default App;
