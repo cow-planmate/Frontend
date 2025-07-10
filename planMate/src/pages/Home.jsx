025ff2b62b306514abfe9dafde88d0d843a389f9
wekfj <kdh01pro@gmail.com>
2025-07-10 23:52:19 +0900

    확축 해결중

--- planMate/src/pages/Home.jsx
+++ planMate/src/pages/Home.jsx
@@ -3,18 +3,19 @@ import Navbar from "../components/Navbar";
 
 function App() {
   return (
-    <div className="min-w-[1200px] min-h-screen overflow-x-auto">
-      <div className="flex flex-col items-center justify-center h-auto font-pretendard">
+    <div className="absolute pr-0 pl-0 right-0 left-0">
+      <div className="text-center h-auto font-pretendard">
         <Navbar isLogin={false} />
       </div>
+
       <div>
         <img src={img1} className="w-full h-[35rem] object-cover" />
       </div>
-      <div className="absolute bottom-0 left-0 right-0 flex flex-col items-center justify-end px-8 pb-20">
-        <div className="w-full max-w-7xl min-w-[1000px] bg-white rounded-xl shadow-2xl p-6 mb-6">
+      <div className="absolute  -bottom-20 left-0 right-0 px-8 pb-0">
+        <div className="w-full max-w-7xl min-w-[1000px] bg-white rounded-xl shadow-2xl p-6 mb-6 mx-auto">
           <div className="grid grid-cols-5 gap-4 items-end min-w-[900px]">
-            <div className="flex flex-col min-w-[160px]">
-              <label className="text-gray-600 text-sm mb-1 font-pretendard whitespace-nowrap">
+            <div className="block min-w-[160px]">
+              <label className="text-gray-600 text-sm mb-1 font-pretendard whitespace-nowrap block">
                 출발지
               </label>
               <input
@@ -23,8 +24,8 @@ function App() {
                 placeholder="출발지 입력"
               />
             </div>
-            <div className="flex flex-col min-w-[160px]">
-              <label className="text-gray-600 text-sm mb-1 font-pretendard whitespace-nowrap">
+            <div className="block min-w-[160px]">
+              <label className="text-gray-600 text-sm mb-1 font-pretendard whitespace-nowrap block">
                 여행지
               </label>
               <input
@@ -33,8 +34,8 @@ function App() {
                 placeholder="여행지 입력"
               />
             </div>
-            <div className="flex flex-col min-w-[160px]">
-              <label className="text-gray-600 text-sm mb-1 font-pretendard whitespace-nowrap">
+            <div className="block min-w-[160px]">
+              <label className="text-gray-600 text-sm mb-1 font-pretendard whitespace-nowrap block">
                 기간
               </label>
               <input
@@ -42,8 +43,8 @@ function App() {
                 className="border-b-2 border-gray-300 pb-2 focus:border-blue-500 focus:outline-none w-full"
               />
             </div>
-            <div className="flex flex-col min-w-[160px]">
-              <label className="text-gray-600 text-sm mb-1 font-pretendard whitespace-nowrap">
+            <div className="block min-w-[160px]">
+              <label className="text-gray-600 text-sm mb-1 font-pretendard whitespace-nowrap block">
                 인원수
               </label>
               <select className="font-pretendard border-b-2 border-gray-300 pb-2 focus:border-blue-500 focus:outline-none w-full">
@@ -54,7 +55,7 @@ function App() {
               </select>
             </div>
 
-            <div className="flex flex-col min-w-[160px]">
+            <div className="block min-w-[160px]">
               <button
                 className="cursor-pointer transition-all bg-[#1344FF] text-white px-8 py-3 rounded-lg
             border-[#1344FF] active:translate-y-[2px] hover:bg-blue-600 shadow-lg w-full font-pretendard whitespace-nowrap"
