024661c0f7c3f618749f4f8ba5980c85170f8910
wekfj <kdh01pro@gmail.com>
2025-07-10 22:42:18 +0900

    임시

--- planMate/src/pages/Mypage.jsx
+++ planMate/src/pages/Mypage.jsx
@@ -1,24 +1,20 @@
-import Navbar from '../components/navbar.jsx';
+import Navbar from "../components/navbar.jsx";
 
 function App() {
   return (
-    <div className='font-pretendard'>
-      <Navbar 
-        isLogin = {false}
-      />
-      <div className='mx-auto w-[1400px] pt-8'>
-        <div className='font-bold text-3xl pb-6'>마이페이지</div>
-        <div className='flex w-full'>
-          <div className='border border-gray-300 rounded-lg w-96 h-full py-5 px-5 mr-5'>
-
-          </div>
-          <div className='border border-gray-300 rounded-lg w-full h-full py-5 px-5'>
+    <div className="font-pretendard">
+      <Navbar isLogin={false} />
+      <div className="mx-auto w-[1400px] pt-8">
+        <div className="font-bold text-3xl pb-6">마이페이지</div>
+        <div className="flex w-full">
+          <div className="border border-gray-300 rounded-lg w-96 h-full py-5 px-5 mr-5"></div>
+          <div className="border border-gray-300 rounded-lg w-full h-full py-5 px-5">
             ?
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
