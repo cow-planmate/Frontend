54bb8fae17adc485e06d9d9e975a8e114b15665e
kwnP <kwangwon0424@naver.com>
2025-07-11 18:29:52 +0900

    일정 생성 여행 정보란 구축 중

--- planMate/src/pages/Create.jsx
+++ planMate/src/pages/Create.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
 import Navbar from '../components/navbar.jsx';
+import PlanInfo from '../components/PlanInfo.jsx';
 
 function App() {
+  const info = {
+    "title": "제목없는 여행1",
+    "person": {
+      "adult": 2,
+      "children": 1,
+    },
+    "departure": "명지대학교 인문캠퍼스 인문기숙사",
+    "travel": "부산",
+    "trans": "대중교통"
+  }
   return (
     <div className='font-pretendard'>
       <Navbar 
         isLogin = {true}
       />
       <div className='mx-auto w-[1400px] pt-8'>
-        안녕하세요
+        <PlanInfo info={info} />
+
       </div>
     </div>
   )
