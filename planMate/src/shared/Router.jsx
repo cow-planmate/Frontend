ae33e0076080c6f731f1e1dec4216eefdf63d99c
wekfj <kdh01pro@gmail.com>
2025-07-13 19:24:22 +0900

    모달창들 ui구현

--- planMate/src/shared/Router.jsx
+++ planMate/src/shared/Router.jsx
@@ -5,7 +5,9 @@ import Complete from "../pages/Complete";
 import Create from "../pages/Create";
 import Mypage from "../pages/Mypage";
 import Signuptest from "../pages/Signuptest";
-import Createtest from "../pages/Createtest";
+import Passwordfindtest from "../pages/Passwordfindtest";
+import Logintest from "../pages/Logintest";
+import Themetest from "../pages/Themetest";
 
 // 2. Router 라는 함수를 만들고 아래와 같이 작성합니다.
 //BrowserRouter를 Router로 감싸는 이유는,
@@ -26,7 +28,9 @@ const Router = () => {
         <Route path="create" element={<Create />} />
         <Route path="mypage" element={<Mypage />} />
         <Route path="signuptest" element={<Signuptest />} />
-        <Route path="createtest" element={<Createtest />} />
+        <Route path="logintest" element={<Logintest />} />
+        <Route path="themetest" element={<Themetest />} />
+        <Route path="passwordfindtest" element={<Passwordfindtest />} />
       </Routes>
     </BrowserRouter>
   );
