e9ef3a57e622045406e3a8adad08d4eca2f50c49
kwnP <kwangwon0424@naver.com>
2025-09-08 00:27:04 +0900

    일정생성 2.0을 만들기 위한 준비과정

---
+++ planMate/src/pages/Create2.jsx
@@ -0,0 +1,15 @@
+// 목표: 최대한 간결하고 작동 잘 되게
+import Navbar from "../components/Navbar";
+
+function App() {
+  return (
+    <div className="font-pretendard">
+      <Navbar />
+      <div className="w-full px-8">
+        안녕하세용
+      </div>
+    </div>
+  )
+}
+
+export default App;
--- planMate/src/shared/Router.jsx
+++ planMate/src/shared/Router.jsx
@@ -9,23 +9,12 @@ import Passwordfindtest from "../pages/Passwordfindtest";
 import Logintest from "../pages/Logintest";
 import Themetest from "../pages/Themetest";
 import Landingpage from "../pages/Landingpage";
-
-// 2. Router 라는 함수를 만들고 아래와 같이 작성합니다.
-//BrowserRouter를 Router로 감싸는 이유는,
-//SPA의 장점인 브라우저가 깜빡이지 않고 다른 페이지로 이동할 수 있게 만들어줍니다!
+import Create2 from "../pages/Create2";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 
-                        Routes안에 이렇게 작성합니다. 
-                        Route에는 react-router-dom에서 지원하는 props들이 있습니다.
-
-                        path는 우리가 흔히 말하는 사용하고싶은 "주소"를 넣어주면 됩니다.
-                        element는 해당 주소로 이동했을 때 보여주고자 하는 컴포넌트를 넣어줍니다.
-                 */}
-
         <Route path="/" element={<Home />} />
         <Route path="complete" element={<Complete />} />
         <Route path="create" element={<Create />} />
@@ -35,6 +24,7 @@ const Router = () => {
         <Route path="themetest" element={<Themetest />} />
         <Route path="passwordfindtest" element={<Passwordfindtest />} />
         <Route path="landingpage" element={<Landingpage />} />
+        <Route path="create2" element={<Create2 />} />
       </Routes>
     </BrowserRouter>
   );
