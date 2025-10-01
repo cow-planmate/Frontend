ccd75a077a41ab1b7f1d213a4d89014536ad98ab
kwnP <kwangwon0424@naver.com>
2025-10-01 20:26:39 +0900

    planName 가변 width 개선

--- planMate/src/pages/Create2.jsx
+++ planMate/src/pages/Create2.jsx
@@ -78,7 +78,7 @@ function App() {
           </div>
         </div>
       :
-        <div className="min-[1464px]:w-[1400px] min-[1464px]:px-0 md:px-8 md:py-6 px-5 py-3 mx-auto">
+        <div className="min-[1464px]:w-[1400px] min-[1464px]:px-0 md:px-8 md:py-6 px-6 py-3 mx-auto">
           안녕하세용
         </div>
       }
